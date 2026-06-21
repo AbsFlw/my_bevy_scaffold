8702db84f6a7fa53d8a83b623981ee8d22ed298a
AbsFlw <47880282+AbsFlw@users.noreply.github.com>
2026-06-21 13:23:03 +0800

    Update my_bevy_new.sh to include Bevy features
    Add Bevy dependencies and configure features in Cargo.toml

--- my_bevy_new.sh
+++ my_bevy_new.sh
@@ -3,9 +3,27 @@ project_name=$1
 cargo new $project_name
 cd $project_name
 
-cargo add bevy -F bevy/dynamic_linking
+cargo add bevy
 
 echo "" >> Cargo.toml
+echo "[features]" >> Cargo.toml
+echo 'default = ["dev_native"]
+dev = [
+    # Improve compile times for dev builds by linking Bevy as a dynamic library.
+    "bevy/dynamic_linking",
+    "bevy/bevy_dev_tools",
+    "bevy/bevy_ui_debug",
+    # Improve error messages coming from Bevy
+    "bevy/track_location",
+]
+dev_native = [
+    "dev",
+    # Enable asset hot reloading for native dev builds.
+    "bevy/file_watcher",
+    # Enable embedded asset hot reloading for native dev builds.
+    "bevy/embedded_watcher",
+]' >> Cargo.toml
+echo "" >> Cargo.toml 
 echo '[profile.dev.package."*"]
 opt-level = 3
 
